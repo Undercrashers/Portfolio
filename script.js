06c7ad30eff74709dd1f3be39a590c111abd04cb
Undercrashers <2306127@kiit.ac.in>
2024-01-27 20:58:07 +0530

    pic

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// var a = document.querySelector("#elem1")
+// var abc = a.getAttribute("data-image")
+// console.log(abc)
+
+var elemC = document.querySelector("#elem-container") 
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".element")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
+// elems.forEach(function(e){
+//    e.addEventListener("mouseenter",function(){
+//      var image = e.getAttribute("data-image")
+//      fixed.style.backgroundImage = `url(${"image"})`
+//    })
+// })
