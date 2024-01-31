6236565bb655b85dce889f456a382d5617f53e02
Undercrashers <2306127@kiit.ac.in>
2024-02-01 00:33:48 +0530

    responsive1

--- script.js
+++ script.js
@@ -7,29 +7,60 @@ const scroll = new LocomotiveScroll({
 // var abc = a.getAttribute("data-image")
 // console.log(abc)
 
-var elemC = document.querySelector("#elem-container") 
-var fixed = document.querySelector("#fixed-image")
-elemC.addEventListener("mouseenter",function(){
-    fixed.style.display = "block"
-})
-elemC.addEventListener("mouseleave",function(){
-    fixed.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".element")
-elems.forEach(function(e){
-    e.addEventListener("mouseenter",function(){
-        var image = e.getAttribute("data-image")
-        fixed.style.backgroundImage = `url(${image})`
+function page4(){
+    var elemC = document.querySelector("#elem-container") 
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter",function(){
+        fixed.style.display = "block"
     })
-})
+    elemC.addEventListener("mouseleave",function(){
+        fixed.style.display = "none"
+    })
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+    var elems = document.querySelectorAll(".element")
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter",function(){
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+function swiper(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+    }
+
+
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("#logo")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+swiper()
+page4()
+menuAnimation()
+loaderAnimation()
